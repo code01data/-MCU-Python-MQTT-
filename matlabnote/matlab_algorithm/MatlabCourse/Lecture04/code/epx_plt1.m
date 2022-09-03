9b9e829a318f945bc5bab965a84680eec354c054
delehub <delehub@gmail.com>
2022-09-04 07:22:24 +0800

    ...

--- matlabnote/matlab_algorithm/MatlabCourse/Lecture04/code/epx_plt1.m
+++
@@ -1,5 +0,0 @@
-function epx_plt1
-    years=1994:2000;
-    income=[8 12 20 22 18 24 27];
-    plot(years,income,'--r*','linewidth',2,'markersize',12)
-end
--- matlabnote/matlab_algorithm/MatlabCourse/Lecture10/code/EU.m
+++
@@ -1,4 +0,0 @@
-%% 欧拉方程求解
-
-[x,y]=eulerpro('doty',0,0.5,1);
-plot(x,y);
